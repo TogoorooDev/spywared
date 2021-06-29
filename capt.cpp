f2d4a7da38874799b9da8344da57a114defe6791
TogoorooDev <robhensley10@gmail.com>
2021-06-29 10:56:04 -0700

    Getting somewhere...

--- capt.cpp
+++ capt.cpp
@@ -5,35 +5,83 @@
 
 using namespace Gdiplus;
 
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
+{
+   UINT  num = 0;          // number of image encoders
+   UINT  size = 0;         // size of the image encoder array in bytes
+
+   ImageCodecInfo* pImageCodecInfo = NULL;
+
+   GetImageEncodersSize(&num, &size);
+   if(size == 0)
+      return -1;  // Failure
+
+   pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+   if(pImageCodecInfo == NULL)
+      return -1;  // Failure
+
+   GetImageEncoders(num, size, pImageCodecInfo);
+
+   for(UINT j = 0; j < num; ++j)
+   {
+      if( wcscmp(pImageCodecInfo[j].MimeType, format) == 0 )
+      {
+         *pClsid = pImageCodecInfo[j].Clsid;
+         free(pImageCodecInfo);
+         return j;  // Success
+      }
+   }
+   free(pImageCodecInfo);
+   return -1;  // Failure
+}
+
+void file_export(HBITMAP bitmap){
+    //Turn the bitmap into a file
+    Gdiplus::GdiplusStartupInput gdiplus_startup_input;
+    ULONG_PTR gdiplus_token;
+    GdiplusStartup(&gdiplus_token, &gdiplus_startup_input, NULL);
+
+    Gdiplus::Bitmap *img = new Gdiplus::Bitmap(bitmap, NULL);
+    CLSID clsid;
+    int ret_val = GetEncoderClsid(L"image/png", &clsid);
+
+    img->Save(L"shot.png", &clsid, NULL);
+    delete img;
+
+    Gdiplus::GdiplusShutdown(gdiplus_token);
+}
+
 void capture(){
-    HDC hdc = GetDC(NULL);
-    HDC hDest = CreateCompatibleDC(hdc);
+    //To be honest, I don't understand this part. I just copied. Forgive me.
 
-    int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-    int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
+    //Create Display Contexts
+    HDC screen_dc = GetDC(NULL);
+    HDC dest_dc = CreateCompatibleDC(screen_dc);
 
-    HBITMAP hbDesktop = CreateCompatibleBitmap(hdc, width, height);
-    SelectObject(hDest, hbDesktop);
+    //Get Screen Dimentions
+    int image_width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
+    int image_height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
 
-    BitBlt(hDest, 0, 0, width, height, hdc, 0, 0, SRCCOPY);
-
-    GetImageEncodersSize();
-
-    ReleaseDC(NULL, hdc);
-}
-
-int genXLen(LPRECT rect){
-    int len;
+    //Make a bitmap image
+    HBITMAP bitmap = CreateCompatibleBitmap(screen_dc, image_width, image_height);
     
-    len = 0;
-    return len;
+    //Select the bitmap into the destination display context
+    HGDIOBJ img_obj = SelectObject(dest_dc, bitmap);
+
+    //Copy the screen into the bitmap
+    BitBlt(dest_dc, 0, 0, image_width, image_height, screen_dc, 0, 0, SRCCOPY);
+
+    file_export(bitmap);
+
+    //Use select again to unlink the bitmap and the destination dc (not sure why you use the same function for that)
+    SelectObject(dest_dc, bitmap);
+
+    //Finish up
+    DeleteDC(dest_dc);
+    ReleaseDC(NULL, screen_dc);
 }
 
-int getYLen(LPRECT rect){
-    int len;
+void init_captured(){
+    capture();    
 
-
-
-    len = 0;
-    return 0;
 }
